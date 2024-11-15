ed609543e68bd8c2334967b840dba5b28d493c83
Matthias Winkler <mwinkler@gmx.ch>
2024-11-16 00:02:57 +0100

    Update rgblcd.ts
    Grove RGB LCD v5 use 0x30 instead of 0x62 for RGB address.
    See https://wiki.seeedstudio.com/Grove-LCD_RGB_Backlight/

--- rgblcd.ts
+++ rgblcd.ts
@@ -230,13 +230,13 @@ namespace rgblcd {
     }
 
     function setRGBReg(reg: number, value: number) {
-        pins.i2cWriteNumber(0x62, reg << 8 | value, NumberFormat.Int16BE)
+        pins.i2cWriteNumber(0x30, reg << 8 | value, NumberFormat.Int16BE)
     }
 }
 
 enum Command {
     LCD_ADDRESS = 0x3E,
-    RGB_ADDRESS = 0x62,
+    RGB_ADDRESS = 0x30,
 
     //Hintergrundfarben Adressen
     REG_RED = 0x04,
